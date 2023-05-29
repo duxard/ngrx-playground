60b196cd57e8678bcb53785f744b70eac4127cb4
duxard <duxard@gmail.com>
2023-05-29 19:25:38 +0300

    Add complex counter with selectors

--- src/app/components/complex-counter/complex-counter.component.ts
+++ src/app/components/complex-counter/complex-counter.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 import { select, Store} from '@ngrx/store';
 import { AppStateInterface } from '../../types/appState.interface';
 import * as ComplexCounterActions from './store/actions';
-import { Observable } from 'rxjs';
-import { valueSelector } from './store/selectors';
+import { Observable, combineLatest, of } from 'rxjs';
+import * as Selectors from './store/selectors';
+import { map, switchMap } from 'rxjs/operators';
 
+
+type ComplexCounterVM = Observable<{
+  count: number;
+  max: number | null;
+  maxedOut: boolean;
+}>;
 
 @Component({
   selector: 'app-complex-counter',
@@ -12,10 +19,24 @@ import { valueSelector } from './store/selectors';
   styleUrls: ['./complex-counter.component.scss']
 })
 export class ComplexCounterComponent {
-  count$: Observable<number>;
+  private readonly count$: Observable<number>;
+  private readonly max$!: Observable<number | null>;
+  readonly viewModel$!: ComplexCounterVM;
 
   constructor(private store: Store<AppStateInterface>) {
-    this.count$ = this.store.pipe(select(valueSelector));
+    this.count$ = this.store.pipe(select(Selectors.valueSelector));
+    this.max$ = this.store.pipe(select(Selectors.maxSelector));
+
+    this.viewModel$ = combineLatest([this.count$, this.max$]).pipe(
+      switchMap(([count, max]) => {
+        const maxedOut = Boolean(max && count >= max);
+        return of([count, max, maxedOut]);
+        // or like this to avoid casting at the end of the method
+        // return of<[number, number | null, boolean]>([count, max, maxedOut]);
+
+      }),
+      map(([count, max, maxedOut]) => ({count, max, maxedOut}))
+    ) as ComplexCounterVM;
   }
 
   increment(): void {
--- src/app/components/complex-counter/complex-counter.model.ts
+++ src/app/components/complex-counter/complex-counter.model.ts
@@ -1,4 +1,4 @@
 export interface ComplexCounterInterface {
   value: number;
-  max: number
+  max: number | null
 }
